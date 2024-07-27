f61463d05fe4dbdb1db65f4ff23b6bb8214f4bb7
Deep Saikia <96719584+saikiaDeep@users.noreply.github.com>
2024-07-27 19:51:02 +0530

    Data extractor added

---
+++ helpers/text_extractor.py
@@ -0,0 +1,21 @@
+import fitz
+import glob
+import os 
+def extract_text_from_pdf(pdf_file_path):
+    doc = fitz.open(pdf_file_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+def get_all_text():
+    result = ""
+    pdf_files = glob.glob(os.path.join('..\data', '*.pdf'))
+    for pdf_file in pdf_files:
+        text = extract_text_from_pdf(pdf_file)
+        result += text
+    return result
+
+
+print(get_all_text())        
+        
